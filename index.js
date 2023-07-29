0de0e97056d3e7af95d8adbf5ebae3bb650599d1
Mahamudul Hasan Zubayer <94388111+zubayer3570@users.noreply.github.com>
2023-07-30 04:56:03 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("express")()
 app.get("/", (req, res) => {
-    res.send({ ip: req.header("x-forwarded-for")[0] })
+    res.send({ ip: req.header("x-forwarded-for") })
 })
 app.listen(5000)

199f9999c6c9bbaa0fb607be5bf520d6698c4705
zubayer <zubayer3570@gmail.com>
2023-07-30 04:39:30 +0600

    hi

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("express")()
 app.get("/", (req, res) => {
-    res.send({ ip1: req.socket.remoteAddress, ip2: req.header("x-forwarded-for") })
+    res.send({ ip1: req.socket.remoteAddress, ip2: req.header("x-forwarded-for"), ip3: req.header("x-real-ip") })
 })
 app.listen(5000)

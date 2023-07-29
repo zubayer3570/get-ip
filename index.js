9019ef189899f0a403525a03e1f079d69c35721d
zubayer <zubayer3570@gmail.com>
2023-07-30 04:20:45 +0600

    hi

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const app = require("express")()
 app.get("/", (req, res) => {
-    // console.log("working fine!")
-    // console.log(res.socket.remoteAddress)
-    res.send({ ip: res.socket.remoteAddress })
+    res.send({ ip: req.socket.remoteAddress })
 })
 app.listen(5000)

ba711badae15a22cf83de16a457f6a4fb79194e2
zubayer <zubayer3570@gmail.com>
2023-07-30 03:58:46 +0600

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("express")()
 app.get("/", (req,res)=>{
     // console.log("working fine!")
-    console.log(res.socket.remoteAddress)
-    res.send({message: "working fine!"})
+    // console.log(res.socket.remoteAddress)
+    res.send({ip: res.socket.remoteAddress})
 })
 app.listen(5000)
